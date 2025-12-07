e4b24360bc75f438e4a917947e0c24fff60d941b
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-12-07 17:10:30 +0100

    Added another example

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "SI/all.hpp"
+#include "SI/everything.hpp"
 using namespace si;
 
 int main()
@@ -8,18 +8,23 @@ int main()
 	auto Marathon = 42.195_km;
 	auto Kiptums_time = 2_h;
 	auto average_speed = Marathon / Kiptums_time;
-	std::cout << "Kiptum's average speed was: " << miles_per_hour(average_speed) << " MPH" << std::endl;
+	std::cout << "Kiptum's average speed was " << miles_per_hour(average_speed) << " MPH." << std::endl;
 
 	// Example 2:
 	mass m = 1_kg;
 	speed c = constant::speed_of_light;
 	energy E = m * c*c;
-	std::cout << "Energy of 1kg mass is: " << joule(E) << " Joule" << std::endl;
+	std::cout << "The potential energy of 1kg mass is: " << joule(E) << " Joule." << std::endl;
 
 	// Example 3:
 	auto H2_frequency = 1420.4057517682_MHz;
 	auto H2_wavelength = formula::wavelength(constant::speed_of_light, H2_frequency);
-	std::cout << "Wavelength of H2 in vacuum is: " << centimeter(H2_wavelength) << " cm" << std::endl;
+	std::cout << "The wavelength of H2 in vacuum is " << centimeter(H2_wavelength) << " cm." << std::endl;
+
+	// Example 4:
+	height Eiffel_Tower = 330_m;
+	auto free_fall_time = sqrt((2.0 * Eiffel_Tower) / constant::gravity_of_Earth);
+	std::cout << "Falling from Eiffel Tower takes " << second(free_fall_time) << " sec." << std::endl;
 
 	return 0;
 }
