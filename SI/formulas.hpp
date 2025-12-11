72ea32beb19ebde94770b81f79269750ca7f2922
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-12-11 11:48:18 +0100

    Improved the formulas

--- SI/formulas.hpp
+++ SI/formulas.hpp
@@ -7,7 +7,8 @@ namespace si
 {
 	namespace formula
 	{
-		auto Pythagoras(si::length a, si::length b)
+		// Calculates the hypotenuse in a right triangle, based on Pythagorean equation a² + b² = c² 
+		si::length hypotenuse_in_triangle(si::length a, si::length b)
 		{
 			return sqrt(a*a + b*b);
 		}
@@ -32,14 +33,24 @@ namespace si
 			return sqrt((2.0 * h) / gravity);
 		}
 
-		si::angle glide_path(si::length horizontalDistance, si::height verticalChange)
+		si::angle glide_path(si::length horizontal_distance, si::height vertical_change)
 		{
-			return atan2(horizontalDistance, verticalChange);
+			return atan2(horizontal_distance, vertical_change);
 		}
 
-		si::height vertical_height(si::angle glidePath, si::length horizontalDistance)
+		si::height vertical_height(si::angle glide_path, si::length horizontal_distance)
 		{
-			return horizontalDistance * tan(glidePath);
+			return horizontal_distance * tan(glide_path);
+		}
+
+		si::length distance_to_brake(si::speed current_speed, si::speed target_speed, si::acceleration deceleration)
+		{
+			return (current_speed * current_speed - target_speed * target_speed) / (2.0 * deceleration);
+		}
+
+		si::acceleration acceleration_for_distance(si::speed current_speed, si::speed target_speed, si::length distance)
+		{
+			return (target_speed * target_speed - current_speed * current_speed) / (2.0 * distance);
 		}
 	}
 }
