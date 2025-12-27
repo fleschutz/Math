980bdb5bb567c95f2da3d8b1600a979964768ab8
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-12-27 17:40:15 +0100

    Updated SI/values/densities.h

--- SI/values/densities.h
+++ SI/values/densities.h
@@ -9,7 +9,28 @@ namespace si
 	{
 		const auto Aluminium = 2700_kg_per_m³;
 		const auto Asphalt = 2120_kg_per_m³;
-		//...
+		const auto Brick = 1800_kg_per_m³;
+		const auto Coal = 1350_kg_per_m³; // (average)
+		const auto Concrete = 2200_kg_per_m³;
+		const auto Copper = 8300_kg_per_m³;
+		const auto Gold = 19300_kg_per_m³;
+		const auto Granite = 2750_kg_per_m³;
+		const auto Ice = 917_kg_per_m³; // (at 0°C)
+		const auto Iron = 7272_kg_per_m³;
+		const auto Lead = 11340_kg_per_m³;
+		const auto Marble = 2770_kg_per_m³; // (average)
+		const auto Nickel = 8666_kg_per_m³;
+		const auto Paper = 700_kg_per_m³;
+		const auto Plexiglas = 1180_kg_per_m³;
+		const auto Sand = 1500_kg_per_m³; // (dry)
+		const auto Sandstone = 2300_kg_per_m³; // (average)
+		const auto Silicon = 2330_kg_per_m³;
+		const auto Silver = 10524_kg_per_m³;
+		const auto Snow = 560_kg_per_m³; // (firm)
+		const auto Tin = 7304_kg_per_m³; 
+		const auto Tunsten = 19300_kg_per_m³;
+		const auto Wool = 100_kg_per_m³;
+		const auto Zinc = 7144_kg_per_m³;
 	}
 }
 
